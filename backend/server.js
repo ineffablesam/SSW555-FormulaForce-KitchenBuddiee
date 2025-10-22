0e157de8c185ba541ed193e6a0929584f939a069
mariaebrahim <128166187+mariaebrahim@users.noreply.github.com>
2025-10-22 13:31:47 -0400

    setting up backend server

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,22 @@ import cors from 'cors';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import authRouter from './routes/auth.js';
+import routes from './routes/auth.js';
+import path from 'path';
 
 /* global process */
 
+
 const app = express();
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//const app = express();
 const PORT = process.env.PORT || 4000;
+
+
 
 app.use(helmet());
 app.use(cors());
@@ -20,12 +31,14 @@ app.get('/', (req, res) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
-  void next;
-  console.error(err);
-  const status = err.status || 500;
-  res.status(status).json({ message: err.message || 'Internal Server Error' });
-});
+// app.use((err, req, res, next) => {
+//   void next;
+//   console.error(err);
+//   const status = err.status || 500;
+//   res.status(status).json({ message: err.message || 'Internal Server Error' });
+// });
+
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Backend server listening on port ${PORT}`);
